0723e8174911c92e799bd7191d7c79b5a27cd50a
kavinkishorej-ui <kavinkishore.j@gmail.com>
2025-09-25 15:15:56 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
         <div className="mb-8 animate-fade-in-up">
           <div className="w-40 h-40 rounded-full mx-auto mb-6 shadow-2xl animate-float overflow-hidden">
             <img
-              src="/Profile.jpg"   // Place Profile.jpg inside public/ folder
+              src="/img/Profile.jpg"   // Place Profile.jpg inside public/ folder
               alt="Kavin Kishore"
               className="w-full h-full object-cover"
             />
